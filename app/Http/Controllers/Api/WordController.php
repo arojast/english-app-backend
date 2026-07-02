a639a3889b31fdeeeac2a779d617e3bf8fe24bd3
Andres Rojas T <arojast95@gmail.com>
2026-07-02 16:08:07 -0600

    Correct read to data API  dictionary

--- app/Http/Controllers/Api/WordController.php
+++ app/Http/Controllers/Api/WordController.php
@@ -13,6 +13,79 @@ use Stichoza\GoogleTranslate\GoogleTranslate;
 
 class WordController extends Controller
 {
+    protected function getWordAPI($word) 
+    {
+        $response = Http::get("https://api.dictionaryapi.dev/api/v2/entries/en/{$word}");
+        $data = [];
+
+        if (!$response->successful()) {
+            return response()->json([
+                'message' => 'Word not found in dictionary'
+            ], 404);
+        }
+
+        $response = $response->json();
+        
+        $data['word'] = $word;
+        $data['meaning'] = $response[0]['meanings'][0]['definitions'][0]['definition'] ?? null;
+        $data['pronunciation'] = $response[0]['phonetic'] ?? ($response[0]['phonetics'][0]['text'] ?? $response[0]['phonetics'][1]['text']);
+
+        if(isset($response[0]['phonetics'][0]['audio']) && !empty($response[0]['phonetics'][0]['audio'])) {
+            $data['audio_url'] = $response[0]['phonetics'][0]['audio'];
+        } else if(isset($response[0]['phonetics'][1]['audio']) && !empty($response[0]['phonetics'][1]['audio'])) {
+            $data['audio_url'] = $response[0]['phonetics'][1]['audio'];
+        } else if(isset($response[0]['phonetics'][2]['audio']) && !empty($response[0]['phonetics'][2]['audio'])) {
+            $data['audio_url'] = $response[0]['phonetics'][2]['audio'];
+        } else {
+            $data['audio_url'] = null;
+        }
+        
+        $data['partOfSpeech'] = $response[0]['meanings'][0]['partOfSpeech'] ?? null;
+        $data['synonyms'] = [];
+        $data['antonyms'] = [];
+
+        // Extract synonyms and antonyms from the API response
+        $synonyms = $response[0]['meanings'][0]['definitions'][0]['synonyms'] ?? [];
+        $antonyms = $response[0]['meanings'][0]['definitions'][0]['antonyms'] ?? [];
+
+        if(isset($response[0]['meanings'][0]['synonyms'])) {
+            $synonyms = array_merge($synonyms, $response[0]['meanings'][0]['synonyms']);
+        }
+
+        if(isset($response[0]['meanings'][1]['synonyms'])) {
+            $synonyms = array_merge($synonyms, $response[0]['meanings'][1]['synonyms']);
+        }
+        
+        // Remove duplicates
+        $synonyms = array_unique($synonyms);
+
+        // Clean structure of array_unique
+        if(!empty($synonyms)) {
+            foreach ($synonyms as $synonym) {
+                $data['synonyms'][] = $synonym;
+            }
+        }
+        
+        if(isset($response[0]['meanings'][0]['antonyms'])) {
+            $antonyms = array_merge($antonyms, $response[0]['meanings'][0]['antonyms']);
+        }
+
+        if(isset($response[0]['meanings'][1]['antonyms'])) {
+            $antonyms = array_merge($antonyms, $response[0]['meanings'][1]['antonyms']);
+        }
+
+        // Remove duplicates
+        $antonyms = array_unique($antonyms);
+
+        // Clean structure of array_unique
+        if(!empty($antonyms)) {
+            foreach ($antonyms as $antonym) {
+                $data['antonyms'][] = $antonym;
+            }
+        }
+
+        return $data;
+    }
     
     public function store(Request $request)
     {
@@ -28,36 +101,13 @@ class WordController extends Controller
         // If not exists → call API
         if (!$word) {
 
-            $response = Http::get("https://api.dictionaryapi.dev/api/v2/entries/en/{$wordText}");
-
-            if (!$response->successful()) {
-                return response()->json([
-                    'message' => 'Word not found in dictionary'
-                ], 404);
-            }
-
-            $data = $response->json();
-
-            $meaning = $data[0]['meanings'][0]['definitions'][0]['definition'] ?? null;
-            $pronunciation = $data[0]['phonetic'] ?? ($data[0]['phonetics'][0]['text'] ?? $data[0]['phonetics'][1]['text']);
-            $audio = $data[0]['phonetics'][0]['audio'] ?? $data[0]['phonetics'][1]['audio'];
-            $partOfSpeech = $data[0]['meanings'][0]['partOfSpeech'] ?? null;
-            $synonyms = $data[0]['meanings'][0]['definitions'][0]['synonyms'] ?? [];
-            $antonyms = $data[0]['meanings'][0]['definitions'][0]['antonyms'] ?? [];
+            $data = $this->getWordAPI($wordText);
 
             //service translate word
             $tr = new GoogleTranslate('es');
-            $translate = $tr->translate($wordText);
+            $data['translate'] = $tr->translate($wordText);
 
-            $word = Word::create([
-                'word' => $wordText,
-                'meaning' => $meaning,
-                'translate' => $translate,
-                'pronunciation' => $pronunciation,
-                'audio_url' => $audio,
-                'synonyms' => json_encode($synonyms),
-                'antonyms' => json_encode($antonyms),
-            ]);
+            $word = Word::create($data);
         }
 
         // Attach to user (no duplicates)
